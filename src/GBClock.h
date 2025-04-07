9fb5ed57da1b15b9d0e711a0cc4f4814ebba299b
gabrielbeguren <gabrielbeguren@outlook.com>
2025-04-07 13:13:56 +0200

    docs

--- src/GBClock.h
+++ src/GBClock.h
@@ -5,20 +5,28 @@
 
 namespace gb
 {
+  //Stop Run Clock, useful for delays and waitings in your Arduino programs.
+  //Uses milliseconds as time measure.
   struct Clock
   {
   public:
 
+    //Start counting.
     void start();
 
+    //Stop counting
     unsigned long stop();
 
+    //Stop and then start counting again.
     unsigned long restart();
 
+    //Check if the clock has reached a mark time (in ms).
     bool check(unsigned long mark);
 
+    //Get ms elapsed since the start.
     unsigned long elapsed();
 
+    //Get if this clock has been started.
     bool started();
 
   private:
--- src/GBFrequency.h
+++ src/GBFrequency.h
@@ -6,20 +6,28 @@
 
 namespace gb
 {
+    //This struct generates a frequency pulse.
     struct Frequency
     {
     public:
 
+        //Set frequency (Hz) as integer value and time high  as float value (1.0 = 100%, 0.0 = 0%)
+        //By default th = 50%
         void setup(int hz, float th = 0.5);
 
+        //Read if pulse is high or low
         bool pulse();
 
+        //Set frequency (Hz) as an integer value
         void setHz(int hz);
 
+        //Get frecuency (Hz) as an integer value
         int getHz();
 
+        //Set time high as float value (1.0 = 100%, 0.0 = 0%)
         void setTh(float th);
 
+        //Get time high as float value
         float getTh();
 
     private:
